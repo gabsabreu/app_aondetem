33d580dccbd219fb8643634dfc6cb25468700b0a
Gabe Abreu de Araujo <g.abreu958@gmail.com>
2021-10-06 16:57:00 -0300

    fix_01

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 // ignore: unused_import
+
+// ignore_for_file: unused_import, duplicate_ignore
+
 import 'package:aondetem/screens/contact_form.dart';
 import 'package:aondetem/screens/contacts_list.dart';
 import 'package:aondetem/screens/dashboard.dart';
@@ -15,11 +18,12 @@ class AondeTem extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
           primaryColor: Colors.pinkAccent,
-          accentColor: Colors.grey,
           buttonTheme: ButtonThemeData(
             buttonColor: Colors.pinkAccent,
             textTheme: ButtonTextTheme.primary,
-          )),
+          ),
+          colorScheme:
+              ColorScheme.fromSwatch().copyWith(secondary: Colors.grey)),
       home: Dashboard(),
     );
   }
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_import
+
 import 'package:aondetem/screens/contacts_list.dart';
 import 'package:flutter/material.dart';
 
@@ -7,9 +9,6 @@ class Dashboard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -18,42 +17,43 @@ class Dashboard extends StatelessWidget {
             padding: const EdgeInsets.all(8.0),
             child: Image.asset('images/aondetem_logo.png'),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: InkWell(
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => ContactList(),
-                  ),
-                );
-              },
-              child: Container(
-                padding: EdgeInsets.all(8.0),
-                height: 100,
-                width: 150,
-                color: Colors.pinkAccent,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Icon(
-                      Icons.people,
-                      color: Colors.white,
-                      size: 24.0,
-                    ),
-                    Text(
-                      'Contacts',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16.0,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          )
+          //botao de acesso a tela de contato
+          // Padding(
+          //   padding: const EdgeInsets.all(8.0),
+          //   child: InkWell(
+          //     onTap: () {
+          //       Navigator.of(context).push(
+          //         MaterialPageRoute(
+          //           builder: (context) => ContactList(),
+          //         ),
+          //       );
+          //     },
+          //     child: Container(
+          //       padding: EdgeInsets.all(8.0),
+          //       height: 100,
+          //       width: 150,
+          //       color: Colors.pinkAccent,
+          //       child: Column(
+          //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          //         crossAxisAlignment: CrossAxisAlignment.start,
+          //         children: <Widget>[
+          //           Icon(
+          //             Icons.people,
+          //             color: Colors.white,
+          //             size: 24.0,
+          //           ),
+          //           Text(
+          //             'Contacts',
+          //             style: TextStyle(
+          //               color: Colors.white,
+          //               fontSize: 16.0,
+          //             ),
+          //           )
+          //         ],
+          //       ),
+          //     ),
+          //   ),
+          // )
         ],
       ),
     );
